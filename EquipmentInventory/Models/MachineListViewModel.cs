6a7c1c07cbb98069e9abafbeecbaa776f22b71bb
Josh Mix <joshuadmix@gmail.com>
2018-02-15 07:51:18 -0500

    Started implementing MVC portion. Will need to be changed quite a bit as this follows the basic protocol described in class, but it's simply a starting point.

---
+++ EquipmentInventory/Models/MachineListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EquipmentInventory.Models
+{
+    public class MachineListViewModel
+    {
+        public List<MachineViewModel> Machines { get; set; }
+        public int TotalMachines { get; set; }
+    }
+}
---
+++ EquipmentInventory/Models/MachineViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EquipmentInventory.Models
+{
+    public class MachineViewModel
+    {
+        public int? EquipId { get; set; }
+        public string EquipMake { get; set; }
+        public string EquipModel { get; set; }
+        public string MakeModel => EquipMake + " " + EquipModel;
+    }
+}
